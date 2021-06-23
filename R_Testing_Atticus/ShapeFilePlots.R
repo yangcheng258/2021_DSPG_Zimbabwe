adf72c58263f41659df7031bfc7735c646281f12
unknown <atticusrex@vt.edu>
2021-06-23 14:22:44 -0400

    Updating Files

--- R_Testing_Atticus/ShapeFilePlots.R
+++ R_Testing_Atticus/ShapeFilePlots.R
@@ -9,10 +9,24 @@ library(rgdal)
 # Loads the raw shapefile
 ZimMap = readOGR(dsn = "D:\\Virginia Tech\\DSPG\\2021_DSPG_Zimbabwe\\R_Testing_Atticus", layer="gadm36_ZWE_2")
 
+districts = ZimMap@data[["NAME_2"]]
+
+stats = sample(1:100, length(districts), replace=TRUE)
+
+fake_data <- matrix(c(districts, stats), ncol = 3)
+
+View(fake_data)
+
+# Merges the shapefile and the district data
+
 
 
 # Plots the final map of Zimbabwe
+
+"
 ggplot(ZimMap, aes(x = long, y = lat, group = group)) + 
   geom_polygon(color = 'white', size = 0.5, fill = "orange") +
   coord_equal() + 
   theme_minimal()
+"
+
