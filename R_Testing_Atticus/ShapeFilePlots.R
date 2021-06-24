0922d41c32799a56da9e4acee82c4ad0ba3b7451
yangcheng <yangcheng@vt.edu>
2021-06-24 10:40:52 -0400

    readOGR

--- R_Testing_Atticus/ShapeFilePlots.R
+++ R_Testing_Atticus/ShapeFilePlots.R
@@ -1,3 +1,7 @@
+setwd("G:/My Drive/PhD/Internship/Zimbabwe/03_Git/2021_DSPG_Zimbabwe/R_Testing_Atticus")
+
+
+
 # IMPORTS
 
 library(ggplot2)
@@ -7,13 +11,15 @@ library(sf)
 
 # SCRIPT
 
-
 # Loads the raw shapefile
-ZimMap = readOGR(dsn = "D:\\Virginia Tech\\DSPG\\2021_DSPG_Zimbabwe\\R_Testing_Atticus", layer="ZimMap")
+ZimMap <-  readOGR(dsn = paste0(getwd(),"/shapefiles"), layer="zwe_admbndl_admALL_zimstat_ocha_itos_20180911")
 
 
+# summary(ZimMap)
+# length(ZimMap)
+# head(ZimMap@data)
 
-
+#you might want to check/replace the "districs"
 stats = sample(1:100, length(districts), replace=TRUE)
 
 fake_data <- matrix(c(districts, stats), ncol = 3)
