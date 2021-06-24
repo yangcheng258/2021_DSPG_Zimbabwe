c9d8446adfa369c62fea537d5b683bf4b52e3a5e
yangcheng <yangcheng@vt.edu>
2021-06-24 12:12:13 -0400

    ZimMap

--- R_Testing_Atticus/ShapeFilePlots.R
+++ R_Testing_Atticus/ShapeFilePlots.R
@@ -15,29 +15,24 @@ library(sf)
 ZimMap <-  readOGR(dsn = paste0(getwd(),"/zwe_admbnda_adm2_zimstat_ocha_20180911"), layer="zwe_admbnda_adm2_zimstat_ocha_20180911")
 
 
-summary(ZimMap8)
-length(ZimMap8)
-head(ZimMap8@data)
-
-
-### you might want to check/replace the "districs" By Yang
-stats = sample(1:100, length(districts), replace=TRUE)
-
-fake_data <- matrix(c(districts, stats), ncol = 3)
-
-View(fake_data)
+summary(ZimMap)
+length(ZimMap)
+head(ZimMap@data)
 
 
 ### 'fortify' the data to get a dataframe format required by ggplot2  By Yang
 library(broom)
-spdf_fortified <- tidy(ZimMap, region = "NAME")
+ZimMap_fortified <- tidy(ZimMap, region = "ADM2_EN")
 
+## Might need to modify the code here, ask Sambath the structure of the data
+stats  <-  sample(1:100, length(ZimMap), replace=TRUE)
+fake_data <- matrix(c(ZimMap, stats), ncol = 3)
+View(fake_data)
 
 
 
 # Merges the shapefile and the district data
-
-ZimMap_tidy = left_join(ZimMap@data, fake_data, by="NAME_2")
+ZimMap_tidy  <-  left_join(ZimMap@data, fake_data, by="ADM2_EN")
 
 
 # Plots the final map of Zimbabwe
@@ -48,4 +43,7 @@ if (FALSE) {
     coord_equal() + 
     theme_minimal()
 }
+
+
+
 
